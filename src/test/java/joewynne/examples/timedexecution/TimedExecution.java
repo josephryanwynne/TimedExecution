206493f1b0597bb273e3434980dd40445c4047a8
Joe Wynne <josephryanwynne@gmail.com>
2014-06-11 19:57:42 +0100

    Setting up wait test

---
+++ src/test/java/joewynne/examples/timedexecution/TimedExecution.java
@@ -0,0 +1,22 @@
+package joewynne.examples.timedexecution;
+
+import org.junit.Test;
+
+/**
+ * Stupid class which just sleeps for some period of time. Just for testing. :)
+ */
+public class TimedExecution 
+{
+
+    @Test
+    public void waitFor() throws InterruptedException{
+        //Time in seconds
+        long waitTimeSeconds = Long.getLong("waitTime", 45);
+        
+        long waitTimeMillis = waitTimeSeconds * 1000;
+        
+        Thread.sleep(waitTimeMillis);
+        
+    }
+    
+}
