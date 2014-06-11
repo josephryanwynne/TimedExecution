6999437bed3e5db3d3ebbe122ebe8ff571366f96
Joe Wynne <josephryanwynne@gmail.com>
2014-06-11 20:40:09 +0100

    Adding the files that I THOUGHT were already added...

---
+++ src/test/java/joewynne/examples/timedexecution/TimedExecutionTest.java
@@ -0,0 +1,23 @@
+package joewynne.examples.timedexecution;
+
+import org.junit.Test;
+
+/**
+ * Stupid class which just sleeps for some period of time. Just for testing. :)
+ */
+public class TimedExecutionTest 
+{
+
+    @Test
+    public void waitFor() throws InterruptedException{
+        //Time in seconds
+        long waitTimeSeconds = Long.getLong("waitTime", 45);
+        
+        long waitTimeMillis = waitTimeSeconds * 1000;
+        
+        System.out.println("Sleeping for " + waitTimeSeconds + " seconds");
+        Thread.sleep(waitTimeMillis);
+        
+    }
+    
+}
